b01fec9e87273687bee9f2fbd4452e1244944ed6
xvhayko <125662794+xvhayko@users.noreply.github.com>
2023-02-27 22:00:46 +0300

    27.02

--- 1.py
+++ 1.py
@@ -1,5 +1,8 @@
-
-# Online Python - IDE, Editor, Compiler, Interpreter
-a=int(input())
-if a> 0:
-    print('сециальное сообщение')
+a = int(input("Введите 3х значное число"))
+if a > 999 or a < 100:
+    print("плохое число")
+else: 
+    a1 = a // 100
+    a2 = a // 10 % 10
+    a3 = a % 10
+print(a - ((a3 * 100) + (a2 * 10) + a1))
--- 2.py
+++ 2.py
@@ -1,8 +1,14 @@
-
-# Online Python - IDE, Editor, Compiler, Interpreter
-a=int(input())
-
-if a>0:
-    print('1')
+a = int(input("Введите 6ти значное число:"))
+if a > 999999 or a < 100000:
+    print("плохое число")
 else:
-    print('-1')
+    a1 = a // 100000
+    a2 = a // 10000 % 10
+    a3 = a // 1000 % 10
+    a4 = a // 100 % 10
+    a5 = a // 10 % 10
+    a6 = a % 10
+if a1 == 2 or a2 == 2 or a3 == 2 or a4 == 2 or a5 == 2 or a6 == 2:
+    print("В числе есть 2")
+else:
+    print("В числе нет 2")
